a9021503fa8dde7e211b1446beff5e666c8155ac
Ikram Maulana <ikram075@ummi.ac.id>
2023-02-10 10:47:52 +0700

    Feat: Dynamic Classes - Dark Card

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -3,11 +3,12 @@ import styles from "../styles/Card.module.scss";
 interface CardProps {
   title: string;
   body: string;
+  dark?: boolean;
 }
 
-export default function Card({ title, body }: CardProps) {
+export default function Card({ title, body, dark = false }: CardProps) {
   return (
-    <div className={styles.card}>
+    <div className={`${styles.card} ${dark && styles.dark}`}>
       <h3>{title}</h3>
       <p>{body}</p>
     </div>
