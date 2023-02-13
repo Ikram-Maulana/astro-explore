0cd6b97b6a3e34a78fb8a08d8571a1fce5c3ca51
Ikram Maulana <ikram075@ummi.ac.id>
2023-02-13 08:55:32 +0700

    Feat: Add Sitemap & Vercel Library

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,14 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 
 // https://astro.build/config
+import sitemap from "@astrojs/sitemap";
+
+// https://astro.build/config
+import vercel from "@astrojs/vercel/serverless";
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  integrations: [react(), sitemap()],
+  output: "server",
+  adapter: vercel()
 });
