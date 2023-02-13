1a8b90a814bf1e1ec5ef1c23c764d8227fe4533b
Ikram Maulana <ikram075@ummi.ac.id>
2023-02-13 07:00:55 +0700

    Feat: Refactor Card Component

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,16 +1,12 @@
 import styles from "../styles/Card.module.scss";
 
 interface CardProps {
-  title: string;
-  body: string;
   dark?: boolean;
+  children: React.ReactNode;
 }
 
-export default function Card({ title, body, dark = false }: CardProps) {
+export default function Card({ dark = false, children }: CardProps) {
   return (
-    <div className={`${styles.card} ${dark && styles.dark}`}>
-      <h3>{title}</h3>
-      <p>{body}</p>
-    </div>
+    <div className={`${styles.card} ${dark && styles.dark}`}>{children}</div>
   );
 }
